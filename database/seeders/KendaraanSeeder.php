8cc5a4a6073d37604f157e4a9661eac348911bf8
Bayu Fanani <fanani707@gmail.com>
2023-06-07 23:06:18 +0700

    update seed not using factory

--- database/seeders/KendaraanSeeder.php
+++ database/seeders/KendaraanSeeder.php
@@ -14,6 +14,37 @@ class KendaraanSeeder extends Seeder
      */
     public function run()
     {
-        Kendaraan::factory(10)->create();
+        Kendaraan::insert([
+            [
+                "tahun_keluaran" => 2012,
+                "warna" => "merah",
+                "harga" => 2000000,
+                "mesin" => "110cc",
+                "tipe_suspensi" => "Suspensi MacPherson Strut",
+                "tipe_transmisi" => "matic",
+                "jenis" => "motor",
+                "stok" => 3,
+            ],
+            [
+                "tahun_keluaran" => 2011,
+                "warna" => "hitam",
+                "harga" => 2100000,
+                "mesin" => "125cc",
+                "tipe_suspensi" => "Suspensi MacPherson Strut",
+                "tipe_transmisi" => "manual",
+                "jenis" => "motor",
+                "stok" => 12,
+            ],
+            [
+                "tahun_keluaran" => 2010,
+                "warna" => "putih",
+                "harga" => 200000000,
+                "mesin" => "1000cc",
+                "tipe_suspensi" => "tidak tau",
+                "tipe_transmisi" => "manual",
+                "jenis" => "mobil",
+                "stok" => 2,
+            ]
+        ]);
     }
 }
